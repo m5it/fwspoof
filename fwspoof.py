2387259ce44aed3c8d94d5dc841e112b128202e6
t3ch <w4d4f4k@gmail.com>
2026-02-07 11:41:45 +0000

    praying :)

--- fwspoof.py
+++ fwspoof.py
@@ -208,6 +208,7 @@ def parse( line:str ):
 	cfto = crc32b(fto)
 	cftt = crc32b(ftt)
 	CDTS = cdts()
+	print("parse() fto({}): {}, ftt({}): {}".format(cfto,ftp,cftt,ftt))
 	#
 	if cfto not in MemoryFlood:
 		#
